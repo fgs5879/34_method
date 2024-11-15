8bde4e51980d30f1f2988c6c9962d522745caba2
fgs5879 <fgss5879@gmail.com>
2024-11-15 18:47:13 +0900

    duplicate_gameの追加

--- utilies.py
+++ utilies.py
@@ -1,20 +1,40 @@
+import copy
 #他の自作pyファイルからimportされる関数を記述する
 def get_playable_cards(hand, energy):
         
         """
         与えられた手札とエナジーに基づいて、使用可能なカードのリストを返す関数。
         """
+        #コストと名前の一致するカードをまとめる。
+        unique_hand = remove_duplicate_cards(hand)
+
         playable_cards = ["end_turn"]
-        strike_cards = [card for card in hand if card.name == "Strike"]
-        non_strike_cards = [card for card in hand if card.name != "Strike"]
-        # 手札を一枚ずつ確認して、コストがエナジー以下のカードをリストに追加
-        #ストライク以外に使用可能なカードが無いか確認
-        for card in non_strike_cards:
-            if card.usable == True and card.cost <= energy and card.name != "Defend":
+        #使用可能カードリストを得る
+        for card in unique_hand:
+            if card.usable == True and card.cost <= energy:
                 playable_cards.append(card)
-        #ストライクしか使用可能カードが無い場合
-        if len(playable_cards) == 1 and len(strike_cards) > 0:
-            for card in strike_cards:
-                if card.usable == True and card.cost <= energy:
-                    playable_cards.append(card)
+
         return playable_cards
+
+def remove_duplicate_cards(card_list):
+    # name と cost を基準に重複を排除
+    seen = set()
+    unique_cards = []
+    
+    for card in card_list:
+        card_key = (card.name, card.cost)  # name と cost を組み合わせてキーに
+        if card_key not in seen:
+            seen.add(card_key)
+            unique_cards.append(card)
+    
+    return unique_cards
+
+def duplicate_game(game):
+    """gameを複製する"""
+    duplicated_game = copy.deepcopy(game)
+    # # リストやオブジェクトを手動で初期化（シャローコピーではなく、ディープコピー）
+    # duplicated_game.hand = copy.deepcopy(game.hand)
+    # duplicated_game.discard_pile = copy.deepcopy(game.discard_pile)
+    # duplicated_game.exhaust_pile = copy.deepcopy(game.exhaust_pile)
+    # duplicated_game.deck = copy.deepcopy(game.deck)
+    return duplicated_game
