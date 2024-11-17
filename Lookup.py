589451c73ec0fbd1a46ad46b2116ae438d00e29b
fgs5879 <fgss5879@gmail.com>
2024-11-17 16:46:59 +0900

    Cardの管理方法修正に合わせて書き換え

--- Lookup.py
+++ Lookup.py
@@ -1,7 +1,7 @@
-import CardGame
+from CardGame import *
 from utilies import get_playable_cards, duplicate_game
-import concurrent.futures
-def lookup(game:CardGame, use_card, lookahead_end_turns):
+import concurrent.futures, random
+def lookup(game:CardGame, used_card, lookahead_end_turns):
     """
     ゲームの状態と選択されたカードを受け取り、
     選択されたカードが使用された場合のゲームをlookup_turnだけ先読みし、先読みの中で与えたダメージを返す。
@@ -11,32 +11,36 @@ def lookup(game:CardGame, use_card, lookahead_end_turns):
     #選択をこのlookup内で実行
     #end_turnが選ばれた場合ターン終了する。lookahead_turnsを超えた場合、この推測での合計ダメージを返す。
     # print(game.turn_num, lookahead_end_turns, game.END_TURN_NUM)
-    if use_card.name == "end_turn":
-        game.end_turn()
+    if used_card["name"] == "end_turn":
+        end_turn(game)
         #終了条件
         if game.turn_num > lookahead_end_turns or game.turn_num > game.END_TURN_NUM:
             # print("end")
-            return (game.total_damage, use_card)
-        game.start_turn()
+            return (game.total_damage, used_card)
+        start_turn(game)
     #そのカードを使用してみる
     else:
-        game.use_card(use_card)
+        use_card(game,used_card)
 
     #使用可能カードリストを得る
     playable_cards = get_playable_cards(game.hand, game.energy)
-
+    #発火+があれば最優先で使用
+    # if any({"name":"Inflame+"}.items() <= c.items() for c in playable_cards):
+    #     return (lookup(duplicate_game(game), {"name":"Inflame+", "cost":1}, lookahead_end_turns)[0], {"name":"Inflame+", "cost":1})
+    # #ダブルタップ+があれば最優先で使用
+    # elif any({"name":"Double_Tap+"}.items() <= c.items() for c in playable_cards):
+    #     return (lookup(duplicate_game(game), {"name":"Double_Tap+", "cost":1}, lookahead_end_turns)[0], {"name":"Double_Tap+", "cost":1})
     #それぞれの選択ごとにゲームの先読みし、先読み先で与えたダメージをlistに格納。最も効果が高い選択を推測する
     #len(playable_cards) <= 1だと、playable_cards = ["end_turn"]であり、ターンエンドするしかない
-    # lookuped_damage_list = []
-    # for played_card in playable_cards:
-    #     simulated_game = duplicate_game(game)
-    #     lookuped_damage_list.append((lookup(simulated_game, played_card, lookahead_end_turns)))
     lookuped_damage_list = []
     for played_card in playable_cards:
-        simulated_game = duplicate_game(game)
-        lookuped_damage_list.append((lookup(simulated_game, played_card, lookahead_end_turns)))
+        #shuffleしない場合、凍った眼を持っているようなものになる
+        # random.shuffle(simulated_game.draw_pile)
+        lookuped_damage_list.append((lookup(duplicate_game(game), played_card, lookahead_end_turns)))
     max_damage, best_choice = max(lookuped_damage_list, key=lambda x: x[0])
-    return (lookup(game, best_choice, lookahead_end_turns)[0], use_card)
+    # print(best_choice)
+    # print(game.energy)
+    return (lookup(game, best_choice, lookahead_end_turns)[0], used_card)
         
 
         
